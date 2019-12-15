d59a2e44adbc3b0daf66e14a778da309d9595d08
TechnoStudy <info@techno.study>
2019-12-15 14:42:18 -0500

    Day 3, working with form finished

--- src/day3/ChromeBrowser.java
+++ src/day3/ChromeBrowser.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
@@ -11,11 +12,11 @@ public class ChromeBrowser {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty( "webdriver.chrome.driver", "D:\\TechnoStudy\\Selenium\\ChromeDriver\\chromedriver.exe" );
         WebDriver driver = new ChromeDriver();
-        driver.get( "file:///D:/Project/SeleniumExercises/src/day3/resources/form.html" );
+        driver.get( "file:///D:/Project/seleniumWorking/src/day3/resources/form.html" );
         WebElement text = driver.findElement( By.name( "text" ) );
         text.sendKeys( "This is my text" );
-        WebElement webElement = driver.findElement( By.name( "password" ) );
-        webElement.sendKeys( "Password" );
+        WebElement password = driver.findElement( By.name( "password" ) );
+        password.sendKeys( "Password" );
         WebElement textarea = driver.findElement( By.name( "textarea" ) );
         textarea.sendKeys( "Some text Area words" );
         WebElement checkbox = driver.findElement( By.name( "checkbox" ) );
@@ -24,7 +25,34 @@ public class ChromeBrowser {
         WebElement secondElementOfRadio = radio.get( 1 );// [0, 1, 2].click
         secondElementOfRadio.click();
 
-        //
+        WebElement select = driver.findElement( By.name( "select" ) );
+        // Select for dropdown
+        Select dropdown = new Select( select );
+//        dropdown.selectByIndex( 1 );
+        List<WebElement> options = dropdown.getOptions();
+        for(WebElement element : options) {
+            if(element.getText().equals( "option 4" ))
+                dropdown.selectByVisibleText( "option 4" );
+        }
+
+        String value1 = text.getAttribute( "value" );
+        String value2 = password.getAttribute( "value" );
+        String value3 = textarea.getAttribute( "value" );
+        String value4 = checkbox.getAttribute( "value" );
+        String value5 = secondElementOfRadio.getAttribute( "value" );
+        String value6 = select.getAttribute( "value" );
+        System.out.println( value1 );
+        System.out.println( value2 );
+        System.out.println( value3 );
+        System.out.println( value4 );
+        System.out.println( value5 );
+        System.out.println( value6 );
+
+        WebElement submitButton = driver.findElement( By.id( "submit" ) );
+        submitButton.click();
+
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println( currentUrl );
 
 // close the drive and associated windows
         driver.quit();
