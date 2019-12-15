c9e7938f141471e82d5d2a3138a3e48317cab24b
TechnoStudy <info@techno.study>
2019-12-15 16:35:25 -0500

    Day 3, task 1 text

--- src/day3/task/SimpleFormTask1.java
+++ src/day3/task/SimpleFormTask1.java
@@ -1,4 +1,7 @@
 package day3.task;
 
 public class SimpleFormTask1 {
+    // https://www.seleniumeasy.com/test/basic-first-form-demo.html
+    // Fill in Single Input Field with text, press the button and check result
+
 }
