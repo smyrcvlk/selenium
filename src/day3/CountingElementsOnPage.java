99d46e41546519dc15b185ba2009045c71e3fddd
TechnoStudy <info@techno.study>
2019-12-15 15:48:57 -0500

    Day 3, counting elements

---
+++ src/day3/CountingElementsOnPage.java
@@ -0,0 +1,30 @@
+package day3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CountingElementsOnPage {
+    public static void main(String[] args) {
+        System.setProperty( "webdriver.chrome.driver", "D:\\TechnoStudy\\Selenium\\ChromeDriver\\chromedriver.exe" );
+        WebDriver driver = new ChromeDriver();
+        driver.get( "file:///D:/Project/seleniumWorking/src/day3/resources/form.html" );
+
+        List<String> tagsForTesting = new ArrayList<>( Arrays.asList( "h1", "h2", "h3", "p", "input", "option", "br", "select" ) );
+        for(String tag : tagsForTesting) {
+            printNumberOfElementsOnAPage( driver, tag );
+        }
+
+        driver.quit();
+    }
+
+    static void printNumberOfElementsOnAPage(WebDriver driver, String tagName) {
+        List<WebElement> elements = driver.findElements( By.tagName( tagName ) );
+        System.out.println( "There is(are) " + elements.size() + " " + tagName + " tag(s) on page!" );
+    }
+}
