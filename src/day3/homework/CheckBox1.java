9626d617d28374692d5edc98abd0fb4107d991cc
TechnoStudy <info@techno.study>
2019-12-15 16:43:38 -0500

    Day 3, homework

---
+++ src/day3/homework/CheckBox1.java
@@ -0,0 +1,6 @@
+package day3.homework;
+
+public class CheckBox1 {
+    // https://www.seleniumeasy.com/test/basic-checkbox-demo.html
+    // Check the Single Checkbox and check the message bellow it
+}
---
+++ src/day3/homework/CheckBox2.java
@@ -0,0 +1,9 @@
+package day3.homework;
+
+public class CheckBox2 {
+    // https://www.seleniumeasy.com/test/basic-checkbox-demo.html
+    // Check the Multiple Checkbox and check the button message
+    // When you check all the checkboxes, button message will change to 'Uncheck All'
+    // When you uncheck at least one checkbox, button message will change to 'Check All'
+
+}
